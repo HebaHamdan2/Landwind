251e2ac7b1e7a11e12161880262946f782f4b825
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-12-27 21:01:26 +0200

    navbar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    fontFamily:{
-     Inter:['Inter','sans-serif'],
-    },
+   
     extend: {
+      screens:{
+        '2md':'960px'
+      },
       colors:{
         primary:'#7E3AF2',
         secondaryText:'#6B7280',
         mainText:'#111928'
-      }
+      }, fontFamily:{
+        Inter:['Inter','sans-serif'],
+       },
     },
   },
   plugins: [],
